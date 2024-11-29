19d9ccf2fd3625f4ef0a255aabebb1bd4d1f4f9f
Emandi Chakri Sai Pavan <95292994+ChakriEmandi@users.noreply.github.com>
2024-11-29 18:41:03 +0530

    Added new files

---
+++ website/components/footer.php
@@ -0,0 +1,41 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <h3>Quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> Home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> About</a>
+         <a href="shop.php"> <i class="fas fa-angle-right"></i> Shop</a>
+         <a href="contact.php"> <i class="fas fa-angle-right"></i> Contact</a>
+      </div>
+
+      <div class="box">
+         <h3>Extra links</h3>
+         <a href="user_login.php"> <i class="fas fa-angle-right"></i> Login</a>
+         <a href="user_register.php"> <i class="fas fa-angle-right"></i> Register</a>
+         <a href="cart.php"> <i class="fas fa-angle-right"></i> Cart</a>
+         <a href="orders.php"> <i class="fas fa-angle-right"></i> Orders</a>
+      </div>
+
+      <div class="box">
+         <h3>Contact Us.</h3>
+         <a href="tel:1234567890"><i class="fas fa-phone"></i> +91 1234567890</a>
+         <a href="tel:9876543210"><i class="fas fa-phone"></i> +91 9876543210</a>
+         <a href="mailto:helpdesk.ebuy.ac.in"><i class="fas fa-envelope"></i> helpdesk.ebuy.ac.in</a>
+         <a href="https://www.google.com/maps"><i class="fas fa-map-marker-alt"></i> Amaravati, India </a>
+      </div>
+
+      <div class="box">
+         <h3>Follow Us</h3>
+         <a href="https://www.facebook.com/" target="_blank"><i class="fab fa-facebook-f"></i>facebook</a>
+         <a href="https://twitter.com/" target="_blank"><i class="fab fa-twitter"></i>Twitter</a>
+         <a href="https://www.instagram.com/" target="_blank"><i class="fab fa-instagram"></i>Instagram</a>
+         <a href="https://www.linkedin.com/in/" target="_blank"><i class="fab fa-linkedin"></i>Linkedin</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>e-Buy</span> | all rights reserved!</div>
+
+</footer>
